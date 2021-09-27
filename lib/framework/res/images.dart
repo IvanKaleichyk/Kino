2e45e2920120534ab1ff9acdfdf8d9d77473a4a8
ivankl <iwan.kaleiczyk@proexe.eu>
2021-09-27 23:38:36 +0200

    setup flutter pub

---
+++ lib/framework/res/images.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ImagesAssets{
+
+  static const String _PATH = "assets/images/";
+
+  static const String APP_LOGO = _PATH + "app_logo.png";
+
+  static const String IC_MORE = _PATH + "ic_more.png";
+  static const String IC_MOVIES = _PATH + "ic_movies.png";
+  static const String IC_PEOPLE = _PATH + "ic_people.png";
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import "package:flutter/material.dart";
+import 'package:kino/core/data/client/accountClient/account_client.dart';
+import 'package:kino/core/data/models/account/account_details.dart';
+import 'package:kino/framework/extensions/image_extensions.dart';
+import 'package:kino/framework/res/images.dart';
+
+import 'di/di.dart';
 
 void main() {
+  configureInjection();
   runApp(MyApp());
 }
 
@@ -8,10 +15,24 @@ class MyApp extends StatelessWidget {
   MyApp({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => MaterialApp(
-        title: "Flutter Demo",
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        )
-      );
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Flutter Demo",
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: TestPage(),
+    );
+  }
+}
+
+class TestPage extends StatelessWidget {
+  const TestPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: getImage(ImagesAssets.APP_LOGO)
+    );
+  }
 }
