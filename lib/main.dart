652a0ff0c88187e8554f8c6790aeb9deeed85abf
ivankl <iwan.kaleiczyk@proexe.eu>
2021-09-27 23:39:23 +0200

    debug code

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import "package:flutter/material.dart";
-import 'package:kino/core/data/client/accountClient/account_client.dart';
-import 'package:kino/core/data/models/account/account_details.dart';
 import 'package:kino/framework/extensions/image_extensions.dart';
 import 'package:kino/framework/res/images.dart';
 
@@ -15,24 +13,19 @@ class MyApp extends StatelessWidget {
   MyApp({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Flutter Demo",
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: TestPage(),
-    );
-  }
+  Widget build(BuildContext context) => MaterialApp(
+        title: "Flutter Demo",
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: TestPage(),
+      );
 }
 
 class TestPage extends StatelessWidget {
   const TestPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: getImage(ImagesAssets.APP_LOGO)
-    );
-  }
+  Widget build(BuildContext context) =>
+      Scaffold(body: getImage(ImagesAssets.APP_LOGO));
 }
