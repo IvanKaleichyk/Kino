f5aaa3b48070788bb2a09786603caa8736e3f204
ivankl <iwan.kaleiczyk@proexe.eu>
2021-09-28 22:42:36 +0200

    setup colors

---
+++ lib/framework/extensions/color_extensions.dart
@@ -0,0 +1,19 @@
+import 'dart:ui';
+
+Color getColor(String hex) => _HexColor.fromHex(hex);
+
+
+extension _HexColor on Color {
+  static Color fromHex(String hexString) {
+    final StringBuffer buffer = StringBuffer();
+    if (hexString.length == 6 || hexString.length == 7) buffer.write('ff');
+    buffer.write(hexString.replaceFirst('#', ''));
+    return Color(int.parse(buffer.toString(), radix: 16));
+  }
+
+  String toHex({bool leadingHashSign = true}) => '${leadingHashSign ? '#' : ''}'
+      '${alpha.toRadixString(16).padLeft(2, '0')}'
+      '${red.toRadixString(16).padLeft(2, '0')}'
+      '${green.toRadixString(16).padLeft(2, '0')}'
+      '${blue.toRadixString(16).padLeft(2, '0')}';
+}
