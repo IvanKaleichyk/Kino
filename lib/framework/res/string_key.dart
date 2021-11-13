d075b7d7cdb9552ecf25c0dbaee66277baa61e69
ivankl <iwan.kaleiczyk@proexe.eu>
2021-11-13 14:21:00 +0100

    add string

--- lib/framework/res/string_key.dart
+++ lib/framework/res/string_key.dart
@@ -1,12 +1,18 @@
-class StringKey {
+typedef StringKey = String;
 
-  static const String APP_NAME = "app_name";
+class StringRes {
 
-  static const String APP_DESCRIPTION = "app_description";
+  static const StringKey APP_NAME = "app_name";
+  static const StringKey APP_DESCRIPTION = "app_description";
+  static const StringKey GET_START = "get_start";
+  static const StringKey WELCOME_TEXT = "welcome_text";
+  static const StringKey SEARCH = "search";
+  static const StringKey CANCEL = "cancel";
+  static const StringKey OK = "ok";
 
-  static const String GET_START = "get_start";
+  static const StringKey DIALOG_WELCOME_TITLE = "dialog_welcome_title";
+  static const StringKey DIALOG_WELCOME_INFO = "dialog_welcome_info";
 
-  static const String WELCOME_TEXT = "welcome_text";
-
-  static const String SEARCH = "search";
+  static const StringKey DIALOG_NOT_GET_ACCESS_TITLE = "dialog_not_get_access_title";
+  static const StringKey DIALOG_NOT_GET_ACCESS_INFO = "dialog_not_get_access_info";
 }
